8135329eacddb6d149164688a08add88dcaca074
Matheus Alamino Martins <matheus.martins@ambevtech.com.br>
2023-08-04 14:12:41 -0300

    Installed and working translater

--- src/app/pages/about-me/about-me.component.ts
+++ src/app/pages/about-me/about-me.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { TranslateService } from '@ngx-translate/core';
 import { IPictureThumbnail } from 'app/interfaces/ipicturethumbnail';
 import { IThumbanail, IVideo } from 'app/interfaces/ivideo';
 import { VimeoApiService } from 'app/services/vimeo-api.service';
@@ -12,87 +13,93 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 })
 export class AboutMeComponent implements OnInit {
 
-    constructor(private vimeoApiService: VimeoApiService,
-        private sanitizer: DomSanitizer) { }
+  constructor(private vimeoApiService: VimeoApiService,
+    private translate: TranslateService) {
+    translate.addLangs(['en', 'fr']);
+    translate.setDefaultLang('en');
 
-    pictures: IPictureThumbnail[] = [];
-    thumbnails: IThumbanail[];
-    customOptions: OwlOptions = {
-      loop: true,
-      margin: 15,
-      mouseDrag: true,
-      touchDrag: true,
-      pullDrag: true,
-      dots: false,
-      navSpeed: 700,
-      autoplay: true,
-      autoplayTimeout: 3000,
-      autoplayHoverPause: true,
-      merge: true,
-      mergeFit: true,
-      autoplaySpeed: 1000,
-      rtl: true,
-      responsive: {
-        0: {
-          items: 1
-        },
-        400: {
-          items: 2
-        },
-        740: {
-          items: 2
-        },
-        940: {
-          items: 3
-        }
+    const browserLang = translate.getBrowserLang();
+    translate.use(browserLang.match(/en|fr/) ? browserLang : 'en');
+  }
+
+  pictures: IPictureThumbnail[] = [];
+  thumbnails: IThumbanail[];
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 15,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    merge: true,
+    mergeFit: true,
+    autoplaySpeed: 1000,
+    rtl: true,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 3
       }
     }
+  }
 
-    customOptions2: OwlOptions = {
-      loop: true,
-      margin: 15,
-      mouseDrag: true,
-      touchDrag: true,
-      pullDrag: true,
-      dots: false,
-      navSpeed: 700,
-      autoplay: true,
-      autoplayTimeout: 2500,
-      autoplayHoverPause: true,
-      startPosition: 4,
-      merge: true,
-      mergeFit: true,
-      autoplaySpeed: 1000,
-      rtl: false,
-      responsive: {
-        0: {
-          items: 1
-        },
-        400: {
-          items: 2
-        },
-        740: {
-          items: 2
-        },
-        940: {
-          items: 3
-        }
+  customOptions2: OwlOptions = {
+    loop: true,
+    margin: 15,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    navSpeed: 700,
+    autoplay: true,
+    autoplayTimeout: 2500,
+    autoplayHoverPause: true,
+    startPosition: 4,
+    merge: true,
+    mergeFit: true,
+    autoplaySpeed: 1000,
+    rtl: false,
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 2
+      },
+      940: {
+        items: 3
       }
     }
+  }
 
-    ngOnInit(): void {
-      this.getAllVideos();
-    }
+  ngOnInit(): void {
+    this.getAllVideos();
+  }
 
-    getAllVideos() {
-      this.vimeoApiService.getThumbnails().subscribe(result => {
-        this.thumbnails = result.data;
-        this.thumbnails.forEach((item, index) => {
-          this.pictures.push({
-            name: this.thumbnails[index]['name'],
-            thumbnail: this.thumbnails[index]['pictures']['sizes'][6]['link']
-          });
-        })
-      });
-    }
+  getAllVideos() {
+    this.vimeoApiService.getThumbnails().subscribe(result => {
+      this.thumbnails = result.data;
+      this.thumbnails.forEach((item, index) => {
+        this.pictures.push({
+          name: this.thumbnails[index]['name'],
+          thumbnail: this.thumbnails[index]['pictures']['sizes'][6]['link']
+        });
+      })
+    });
+  }
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,7 +9,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'ngx-owl-carousel-o';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
 
+// AoT requires an exported function for factories
+export function HttpLoaderFactory(httpClient: HttpClient) {
+  return new TranslateHttpLoader(httpClient);
+}
 
 @NgModule({
   declarations: [
@@ -24,7 +31,15 @@ import { CarouselModule } from 'ngx-owl-carousel-o';
     NgbModule,
     RouterModule,
     CarouselModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      }
+    })
   ]
 })
 export class PagesModule { }
