dfa51baa87c8d8f662e3f38534d901a2ae3d352f
Matheus Alamino Martins <matheus.martins@ambevtech.com.br>
2023-08-04 13:47:42 -0300

    update carousel

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,24 +1,22 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AboutMeComponent } from './about-me/about-me.component';
 import { ContactComponent } from './contact/contact.component';
 import { ReelComponent } from './reel/reel.component';
 import { AnimationComponent } from './animation/animation.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { CarouselModule } from 'ngx-owl-carousel-o';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
-
-
 
 
 @NgModule({
   declarations: [
-      AboutMeComponent,
-      ContactComponent,
-      ReelComponent,
-      AnimationComponent
+    AboutMeComponent,
+    ContactComponent,
+    ReelComponent,
+    AnimationComponent
   ],
   imports: [
     CommonModule,
