4a1f532affe41cf0bc5e3a4a26b8cd801ae9da60
Matheus Alamino Martins <matheus.martins@ambevtech.com.br>
2023-08-04 13:47:27 -0300

    Update carousel

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { NouisliderModule } from 'ng2-nouislider';
 import { JwBootstrapSwitchNg2Module } from 'jw-bootstrap-switch-ng2';
 import { RouterModule } from '@angular/router';
 
@@ -14,27 +13,28 @@ import { ComponentsComponent } from './components.component';
 import { NotificationComponent } from './notification/notification.component';
 import { NgbdModalComponent } from './modal/modal.component';
 import { NgbdModalContent } from './modal/modal.component';
+import { NouisliderModule } from 'ng2-nouislider';
 
 @NgModule({
-    imports: [
-        CommonModule,
-        FormsModule,
-        NgbModule,
-        NouisliderModule,
-        RouterModule,
-        JwBootstrapSwitchNg2Module
-    ],
-    declarations: [
-        ComponentsComponent,
-        BasicelementsComponent,
-        NavigationComponent,
-        TypographyComponent,
-        NucleoiconsComponent,
-        NotificationComponent,
-        NgbdModalComponent,
-        NgbdModalContent
-    ],
-    entryComponents: [NgbdModalContent],
-    exports:[ ComponentsComponent ]
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgbModule,
+    NouisliderModule,
+    RouterModule,
+    JwBootstrapSwitchNg2Module
+  ],
+  declarations: [
+    ComponentsComponent,
+    BasicelementsComponent,
+    NavigationComponent,
+    TypographyComponent,
+    NucleoiconsComponent,
+    NotificationComponent,
+    NgbdModalComponent,
+    NgbdModalContent
+  ],
+  entryComponents: [NgbdModalContent],
+  exports: [ComponentsComponent]
 })
 export class ComponentsModule { }
